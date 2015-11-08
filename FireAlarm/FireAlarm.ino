460b53b986dc8077be57ce75216a489c694869ce
Alex <alexander.w.baer@gmail.com>
2015-11-08 15:26:22 +0000

    Fire Alarm Program
    Just a basic fire alarm that displays LCD text and flashing lights when
    the button is pushed. Uses basic LCD and two LEDS with a push button
    switch.

---
+++ FireAlarm/FireAlarm.ino
@@ -0,0 +1,51 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int fireSwitch = 0;
+int scroll = 0;
+const int switchPin = 13;
+const int light1Pin = 8;
+const int light2Pin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(switchPin, INPUT);
+  pinMode(light1Pin, OUTPUT);
+  pinMode(light2Pin, OUTPUT);
+  
+  digitalWrite(light1Pin, LOW);
+  digitalWrite(light2Pin, LOW);
+  
+  lcd.begin(16, 2);
+  lcd.print("Safe");
+  
+}
+
+void loop() {
+  fireSwitch = digitalRead(switchPin);
+  
+  if(fireSwitch == LOW){
+    lcd.begin(16, 2);
+    lcd.print("Safe");
+    digitalWrite(light1Pin, LOW);
+    digitalWrite(light2Pin, LOW);
+  }
+  
+  else{
+    
+    for(int scroll = 0; scroll<16; scroll++){
+      lcd.clear();
+      lcd.setCursor(scroll, 0);
+      lcd.print("FIRE!");
+      if(scroll % 2){
+        digitalWrite(light1Pin, LOW);
+        digitalWrite(light2Pin, HIGH);
+      }
+      else{
+        digitalWrite(light1Pin, HIGH);
+        digitalWrite(light2Pin, LOW);
+      }
+     delay(250);      
+    }
+  }   
+ }
