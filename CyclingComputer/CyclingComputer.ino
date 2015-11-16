1d826020f1bb803489a14665088d4c7ce48560bc
Alex <alexander.w.baer@gmail.com>
2015-11-16 20:51:20 +0000

    Minor Updates
    A few changes this evening. Nothing major but starting to get an idea of
    how we'll change a pause/start

--- CyclingComputer/CyclingComputer.ino
+++ CyclingComputer/CyclingComputer.ino
@@ -4,6 +4,7 @@ unsigned long StartTime;
 unsigned long ElapsedTime;
 int Speed = 0;
 int AverageSpeed = 0;
+boolean Timing = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,22 +16,46 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  
   ElapsedTime = millis() - StartTime
   
-  if (ResetPin == HIGH && ElapsedTime != 0){
-    ElapsedTime = 0
-    delay(50);
+  while(Timing = false){
+    
+    display();
+    
+    if (ResetPin == HIGH && ElapsedTime != 0){
+      ElapsedTime = 0;
+      delay(50);
+    }
+    
+    if (PausePin == HIGH && ElapsedTime !=0){
+      Timing = true;
+      //we need a way to resume the timer
+      delay(50);
+    }
+    
+    
+    if(PausePin == HIGH && ElapsedTime =0){
+      StartTime = millis();
+      Timing = true;
+      delay(50);
+    }
+
   }
   
-  if (PausePin == HIGH && ElapsedTime = 0){
-    StartTime = millis();
+  while(timing = true){
+    display();
+    
+    if (PausePin == HIGH && ElapsedTime != 0){
+    Timing = false;
     delay(50);
+    //We need a way to pause the timer here
+    }
   }
-  
-    
-    
-    
 
+}
 
-
+void display(){
+  //put somethings here to organise our display
 }
