8b824802e49036a7b5fc524f77d0a2876bd93cfd
Alex <alexander.w.baer@gmail.com>
2015-11-19 21:39:56 +0000

    Completed Stop Watch Functionality and Improved Display
    Stop watch functionality is complete. Fixed a few silly errors in the
    logic statements and away we go. Increase delay time to help with
    debounce issue. Created refresh rate parameter for screen to stop the
    insane refresh rate that was every clock cycle! Added milliseconds.
    Display still needs to be properly formatted with leading zeros etc

--- CyclingComputer/CyclingComputer.ino
+++ CyclingComputer/CyclingComputer.ino
@@ -3,6 +3,8 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int ResetPin = 8;
 const int PausePin = 9;
+const long screenrefresh = 50;
+unsigned long previousrefresh = 0;
 unsigned long StartTime;
 unsigned long ElapsedTime;
 unsigned long PauseTimeStart;
@@ -14,48 +16,49 @@ boolean Timing = false;
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
+int milliseconds = 0;
 
 void setup() {
   // put your setup code here, to run once
-  Serial.begin(9600);
   lcd.begin(16, 2);
   pinMode(ResetPin, INPUT);
   pinMode(PausePin, INPUT);
   ElapsedTime = 0;
   TotalPauseTime = 0;
+  Timing = false;
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     
-  while(Timing = false){
+  while(Timing == false){
     
     display();
     
     if (digitalRead(ResetPin) == HIGH && ElapsedTime != 0){
       ElapsedTime = 0;
       TotalPauseTime = 0;
-      delay(50);
+      delay(150);
     }
     
     if (digitalRead(PausePin) == HIGH && ElapsedTime !=0) {
       PauseTime = millis() - PauseTimeStart;
       TotalPauseTime = TotalPauseTime + PauseTime;
       Timing = true;
-      delay(50);
+      delay(150);
     }
     
     
     if (digitalRead(PausePin) == HIGH && ElapsedTime == 0){
       StartTime = millis();
       Timing = true;
-      delay(50);
+      delay(150);
     }
 
   }
   
-  while(Timing = true){
+  while(Timing == true){
     
     ElapsedTime = millis() - (StartTime + TotalPauseTime); 
     
@@ -64,24 +67,31 @@ void loop() {
     if (digitalRead(PausePin) == HIGH && ElapsedTime != 0){
       PauseTimeStart = millis();
       Timing = false;
-      delay(50);
+      delay(150);
    }
   }
 
 }
 
 void display(){
-  seconds = (ElapsedTime / 1000) % 60;
-  minutes = (ElapsedTime / (1000*60)) % 60;
-  hours = (ElapsedTime / (1000*60*60)) % 24;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(hours);
-  lcd.setCursor(3, 0);
-  lcd.print(minutes);
-  lcd.setCursor(6, 0);
-  lcd.print(seconds);
-  Serial.println(Timing);
+  unsigned long currentrefresh = millis();
+  
+  if (currentrefresh - previousrefresh >= screenrefresh){
+    previousrefresh = currentrefresh;
+    milliseconds = (ElapsedTime % 1000);
+    seconds = (ElapsedTime / 1000) % 60;
+    minutes = (((ElapsedTime / 1000)/ 60) % 60);
+    hours = ((((ElapsedTime / 1000) / 60) / 60) % 24);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(hours);
+    lcd.setCursor(2, 0);
+    lcd.print(minutes);
+    lcd.setCursor(4, 0);
+    lcd.print(seconds);
+    lcd.setCursor(7, 0);
+    lcd.print(milliseconds);  
+  }
 }
   
   //This is just a trial display set-up
