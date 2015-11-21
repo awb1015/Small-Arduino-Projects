0af3df7abc1e44d5c391bb762d17b9e232a41bcf
Alex <alexander.w.baer@gmail.com>
2015-11-21 13:11:14 +0000

    Properly formatted Stopwatch Display, functionally complete
    Stop watch functionality is complete with better formatted display. Next
    on to speed, average speed distance and a pretty display

--- CyclingComputer/CyclingComputer.ino
+++ CyclingComputer/CyclingComputer.ino
@@ -78,22 +78,52 @@ void display(){
   
   if (currentrefresh - previousrefresh >= screenrefresh){
     previousrefresh = currentrefresh;
+    //this way the displayed values of time are only recalculated when the display is refreshed
     milliseconds = (ElapsedTime % 1000);
     seconds = (ElapsedTime / 1000) % 60;
     minutes = (((ElapsedTime / 1000)/ 60) % 60);
     hours = ((((ElapsedTime / 1000) / 60) / 60) % 24);
+    //this is just a basic display setup for now
     lcd.clear();
-    lcd.setCursor(0, 0);
+    lcd.setCursor(1, 0);
     lcd.print(hours);
+        
     lcd.setCursor(2, 0);
-    lcd.print(minutes);
-    lcd.setCursor(4, 0);
-    lcd.print(seconds);
-    lcd.setCursor(7, 0);
-    lcd.print(milliseconds);  
+    lcd.print(":");
+    if (minutes < 10){
+      lcd.print("0");      
+      lcd.print(minutes);
+    }
+    else{
+      lcd.print(minutes);
+    }
+    
+    lcd.setCursor(5, 0);
+    lcd.print(":");
+    if (seconds < 10){
+      lcd.print("0");      
+      lcd.print(seconds);
+    }
+    else{
+      lcd.print(seconds);
+    }
+    
+    lcd.setCursor(8, 0);
+    lcd.print(":");
+    if (milliseconds < 10){
+      lcd.print("00");      
+      lcd.print(milliseconds);
+    }
+    if (milliseconds > 9 && milliseconds < 100){
+      lcd.print("0");      
+      lcd.print(milliseconds);
+    }
+    if (milliseconds > 99){
+      lcd.print(milliseconds);
+    }
   }
 }
   
-  //This is just a trial display set-up
+  
   
 
