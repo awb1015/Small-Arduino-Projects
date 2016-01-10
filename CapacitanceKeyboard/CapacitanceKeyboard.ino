83e02d29d07665c125f35909ac0486cd49f6aa79
Alex <alexander.w.baer@gmail.com>
2016-01-10 17:29:17 +0000

    A bit more work
    A bit more complete. I need to fix the for() loop that declares the
    names of capacitance sensors but at the moment I'm not sure of the
    correct way to do so. To be continued!

--- CapacitanceKeyboard/CapacitanceKeyboard.ino
+++ CapacitanceKeyboard/CapacitanceKeyboard.ino
@@ -1,6 +1,6 @@
 #include <CapacitiveSensor.h>
 int threshold = 1000;
-const int NumberofKeys = 4;
+const int NumberofKeys = 2;
 
 for(int i=1; i = NumberofKeys; i++){
   CapacitiveSensor capSensor_i = CapacitiveSensor(13, i+1);
@@ -16,17 +16,21 @@ void setup() {
     pinMode(NumberofKeys + 1 + i, OUTPUT);
     //sets enough output LEDs for HIL support
   }
-  
-      
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   for(int i=1; i = NumberofKeys; i++){
-    //Check sensor value
-    //then illuminate appropriate LED
-    //or turn off LED as needed
-  }
-  
+    long sensorValue = capSensor_i.capacitive.Sensor(30);
+    if(sensorValue > threshold){
+      digitalWrite(NumberofKeys + 1 + i, HIGH);
+    }
+    else{
+      digitalWrite(NumberofKeys + 1 + i, LOW);
+    }
+    
+    delay(10);
+  }    
 }
