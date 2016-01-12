b5b24e1bb977229f5b3747eec259c9ae00030c85
Alex <alexander.w.baer@gmail.com>
2016-01-12 21:01:44 +0000

    More work to do!
    Changed number of keys from a constant int to a macro in the hopes of
    allowing this to compile. As it won't change during running a heap
    overflow isn't a worry but I fear the capacitance library isn't up to
    allowing arrays, or very likely, I'm missing something

--- CapacitanceKeyboard/CapacitanceKeyboard.ino
+++ CapacitanceKeyboard/CapacitanceKeyboard.ino
@@ -1,32 +1,31 @@
 #include <CapacitiveSensor.h>
+#define NUMBER_OF_KEYS 2
 int threshold = 1000;
-const int NumberofKeys = 2;
-CapacitiveSensor capSensor[NumberofKeys];
+int i;
+CapacitiveSensor capSensor[NUMBER_OF_KEYS];
 
 void setup() {
-    
-  for(int i=0; i < NumberofKeys - 1; i++){
+
+  for(int i=0; i < NUMBER_OF_KEYS - 1; i++){
     CapacitiveSensor capSensor[i] = CapacitiveSensor(13, i + 2);
   }
     
-  for(int i=1; i = NumberofKeys; i++){
-    pinMode(NumberofKeys + i, OUTPUT);
-    //sets enough output LEDs for HIL support
+  for(int i=1; i = NUMBER_OF_KEYS; i++){
+    pinMode(NUMBER_OF_KEYS + i, OUTPUT);
   }
    
 }
 
 void loop() {
-  int i;
   long sensorValue;
   
-  for(int i=0; i = NumberofKeys; i++){
+  for(int i=0; i = NUMBER_OF_KEYS; i++){
     sensorValue = capSensor[i].capacitiveSensor(30);
     if(sensorValue > threshold){
-      digitalWrite(NumberofKeys + 1 + i, HIGH);
+      digitalWrite(NUMBER_OF_KEYS + 1 + i, HIGH);
     }
     else{
-      digitalWrite(NumberofKeys + 1 + i, LOW);
+      digitalWrite(NUMBER_OF_KEYS + 1 + i, LOW);
     }
     
     delay(10);
