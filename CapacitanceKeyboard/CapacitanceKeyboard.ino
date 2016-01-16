bc3d2473ba81fd02fdeba1d1d2ef72da3d75ac4b
Alex <alexander.w.baer@gmail.com>
2016-01-16 17:20:25 +0000

    Now with Pointers!
    Compile but still not right. A serial monitor shows there isn't much
    life in our sensors. More work is needed

--- CapacitanceKeyboard/CapacitanceKeyboard.ino
+++ CapacitanceKeyboard/CapacitanceKeyboard.ino
@@ -1,26 +1,27 @@
 #include <CapacitiveSensor.h>
 #define NUMBER_OF_KEYS 2
-int threshold = 1000;
+int threshold = 300;
 int i;
-CapacitiveSensor capSensor[NUMBER_OF_KEYS];
+CapacitiveSensor* capSensor[NUMBER_OF_KEYS];
 
 void setup() {
+  Serial.begin(9600);
 
   for(int i=0; i < NUMBER_OF_KEYS - 1; i++){
-    CapacitiveSensor capSensor[i] = CapacitiveSensor(13, i + 2);
+    capSensor[i] = new CapacitiveSensor(13, i + 2);
   }
     
   for(int i=1; i = NUMBER_OF_KEYS; i++){
-    pinMode(NUMBER_OF_KEYS + i, OUTPUT);
+    pinMode(NUMBER_OF_KEYS + i + 1, OUTPUT);
   }
    
 }
 
 void loop() {
   long sensorValue;
-  
-  for(int i=0; i = NUMBER_OF_KEYS; i++){
-    sensorValue = capSensor[i].capacitiveSensor(30);
+  Serial.println(sensorValue);
+  for(int i=0; i < NUMBER_OF_KEYS; i++){
+    sensorValue = capSensor[i]->capacitiveSensor(30);
     if(sensorValue > threshold){
       digitalWrite(NUMBER_OF_KEYS + 1 + i, HIGH);
     }
