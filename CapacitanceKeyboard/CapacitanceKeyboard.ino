a1962b3eb075310a5d831edc14ed730c2a414035
Alex <alexander.w.baer@gmail.com>
2016-01-26 21:44:08 +0000

    Still working
    Still struggling. I think ultimately the loop() section will need a
    re-write. I'm reasonably confident in setup() but a more thorough
    debugging tomorrow will check that assumption.

--- CapacitanceKeyboard/CapacitanceKeyboard.ino
+++ CapacitanceKeyboard/CapacitanceKeyboard.ino
@@ -8,12 +8,8 @@ void setup() {
 
   for(int i=0; i < NUMBER_OF_KEYS; i++){
     capSensor[i] = new CapacitiveSensor(13, i + 2);
-    if(i== NUMBER_OF_KEYS - 1){
-      i = 0;
     }
-  }
-  
-    
+     
   for(int j=1; j = NUMBER_OF_KEYS; j++){
     pinMode(NUMBER_OF_KEYS + j + 1, OUTPUT);
   }
@@ -22,8 +18,7 @@ void setup() {
 
 void loop() {
   long sensorValue;
-  int x = 1;
-  for(int k=0; k < NUMBER_OF_KEYS; k = k + x){
+  for(int k=0; k < NUMBER_OF_KEYS; k++){
     sensorValue = capSensor[k]->capacitiveSensor(30);
     Serial.println(sensorValue);
     if(sensorValue > threshold){
@@ -34,9 +29,5 @@ void loop() {
     }
     
     delay(10);
-    
-    if (k == NUMBER_OF_KEYS - 1){
-      x = -x;
-    }
   }
 }
