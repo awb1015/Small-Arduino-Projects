3c8b0f77d3905c1ae5d6d7e91573f00cab130d91
Alex <alexander.w.baer@gmail.com>
2016-04-18 21:01:00 +0100

    More corrections
    Fixed some pretty pathetic errors.

--- FanBench/FanBench.ino
+++ FanBench/FanBench.ino
@@ -3,33 +3,35 @@ int NumberofFans = 2;
 
 const int NumberofSwitches = 2;
 const int FanTime = 600000;
-const int RadRedLEDPin = 8;
-const int RadBlueLEDPin = 9;
-const int ICRedLEDPin = 10;
-const int ICBlueLEDPin = 11;
+int RadRedLEDPin = 8;
+int RadBlueLEDPin = 9;
+int i;
+int j;
+int ICRedLEDPin = 10;
+int ICBlueLEDPin = 11;
 unsigned long ICFanTime;
 unsigned long RadFanTime;
-bol RadSwitchState;
-bol ICSwitchState;
-bol previousRadSwitchState;
-bol previousICSwitchState;
+bool RadSwitchState;
+bool ICSwitchState;
+bool previousRadSwitchState;
+bool previousICSwitchState;
  
 void setup() {
   // Create two switch Pins
   for(int j=1; j == NumberofSwitches; i++){
-     PinMode(J + NumberofFans + 1, INPUT);
+     pinMode(j + NumberofFans + 1, INPUT);
      //this makes 4 and 5
   }
  
   //Create Fan Pins
   for(int i=1; i == NumberofFans; i++){
-    PinMode(i+1, OUTPUT);
+    pinMode(i+1, OUTPUT);
     // this makes 2 and 3
   } 
   
   //Create LED Pins, set to off
   for(int k=8; k == 11; k++){
-    PinMode(k, OUTPUT);
+    pinMode(k, OUTPUT);
     digitalWrite(k, LOW);
   }
  
@@ -39,25 +41,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // lose some element of parametric code by refering to specific pins, pointers could help here if I could ever master them
-  radSwitchState = digitalRead(4);
+  RadSwitchState = digitalRead(4);
   ICSwitchState = digitalRead(5);
  
- if(radSwitchState == HIGH && previousRadSwitchState == LOW && RadFanTime == 0){
-   millis() = RadFanTime;
+ if(RadSwitchState == HIGH && previousRadSwitchState == LOW && RadFanTime == 0){
+   RadFanTime = millis();
    digitalWrite(RadRedLEDPin, HIGH);
    digitalWrite(2, HIGH);
    //begin timing sequence
    //begin red light for radiator
  }
  
- if(radSwitchState == HIGH && previousRadSwitchState == HIGH && (RadFanTime - millis()) > FanTime){
-   digitalWrite(RadRedLEDPin = LOW);
-   digitalWrite(RadBlueLEDPin = LOW);
+ if(RadSwitchState == HIGH && previousRadSwitchState == HIGH && (RadFanTime - millis()) > FanTime){
+   digitalWrite(RadRedLEDPin, LOW);
+   digitalWrite(RadBlueLEDPin, LOW);
    //turn off red LED
    //Blue LED on, fans stay on
  }       
  
- if(radSwitchState == LOW && previousRadSwitchState == HIGH){
+ if(RadSwitchState == LOW && previousRadSwitchState == HIGH){
    RadFanTime = 0;
    digitalWrite(RadRedLEDPin, LOW);
    digitalWrite(RadBlueLEDPin, LOW);
@@ -65,7 +67,7 @@ void loop() {
  }
 
 if(ICSwitchState == HIGH && previousICSwitchState == LOW && ICFanTime == 0){
-   millis() = ICFanTime;
+   ICFanTime = millis();
    digitalWrite(ICRedLEDPin, HIGH);
    digitalWrite(3, HIGH);
    //begin timing sequence
@@ -73,8 +75,8 @@ if(ICSwitchState == HIGH && previousICSwitchState == LOW && ICFanTime == 0){
  }
  
 if(ICSwitchState == HIGH && previousICSwitchState == HIGH && (ICFanTime - millis()) > FanTime){
-   digitalWrite(ICRedLEDPin = LOW);
-   digitalWrite(ICBlueLEDPin = LOW);
+   digitalWrite(ICRedLEDPin, LOW);
+   digitalWrite(ICBlueLEDPin, LOW);
    //turn off red LED
    //Blue LED on, fans stay on
  }
@@ -86,8 +88,8 @@ if(ICSwitchState == LOW && previousICSwitchState == HIGH){
    digitalWrite(3, LOW);
  }
  
- previousRadSwitchState = radSwitchState;
+ previousRadSwitchState = RadSwitchState;
  previousICSwitchState = ICSwitchState;
- //need to add IC bench as well
+ 
  
 }
