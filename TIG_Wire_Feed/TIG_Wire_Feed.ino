9645816e5b6f078034113ef5367027e46d54a483
Alex <alexander.w.baer@gmail.com>
2016-06-03 18:51:53 +0100

    Small additions
    Added a bit more. I think we're starting to get there. Need to implement
    the potentiometers for adjust still. Once that is done then on to
    building a circuit and debugging

--- TIG_Wire_Feed/TIG_Wire_Feed.ino
+++ TIG_Wire_Feed/TIG_Wire_Feed.ino
@@ -1,8 +1,8 @@
 #include <Stepper.h>
 const int stepsPerRevolution = 200
 const int stepPin = 3;
-const int maxStepInterval = 2000;
 const int minStepInterval = 50;
+const int maxStepInterval = 2000;
 int stepInterval;
 const int minStepSpeed = 25;
 const int maxStepSpeed = 200;
@@ -13,25 +13,47 @@ int stepSize;
  
 stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 //creates a new stepper object named myStepper
-
+ 
 void setup() {
   // put your setup code here, to run once:
+ 
   PinMode(stepPin, OUTPUT);
+ 
   //set-up pins for potentiometers for speed, size and frequency
+ 
+ 
+ 
   Serial.begin(9600);
   myStepper.setSpeed(stepSpeed);
-  //maybe this should be in loop as step speed will change
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  while(digitalRead(stepPin == HIGH)){
-  //check step conditions
-  //then step motor
-  //and continue loop
-  //output values of three parameters to serial monitor
-  //a while loop may not be best as we want to continue the serial output even when the feed is not on
  
 }
-
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  //output our parameters and their values
+  Serial.print(“stepSpeed”);
+  Serial.print(stepSpeed);
+  Serial.print(“stepInterval”);
+  Serial.print(stepInterval);
+  Serial.print(“stepSize”);
+  Serial.println(stepSize);
+ 
+  //now map our three potentiometers to the new values of stepSize, stepSpeed, and stepInterval;
+  stepInterval =  map(potvalue, lowpot, highpot, minStepInterval, maxStepInterval );
+  stepSpeed = map(potvalue, lowpot, highpot, minStepSpeed, maxStepSpeed);
+  stepSize =  map(potvalue, lowpot, highpot, minStepSize, maxStepSize);
+ 
+  //now we update the speed field of the stepper object
+  myStepper.setSpeed(stepSpeed);
+ 
+  //check that delay time has passed 
+ 
+  //if step pin is pushed and delay time has passed step again
+  if(digitalRead(stepPin)==HIGH){
+  //command to step motor at given speed, for given step size
+  }
+ 
+ 
+ 
 }
